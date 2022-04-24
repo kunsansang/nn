defd1957143c588ea2033fc309d526bd97ba52c9
gearboz <kunsansang@gmail.com>
2022-04-24 12:53:38 +0700

    new

--- htdocs/demo/wp-content/plugins/mailpoet/assets/dist/js/marketing_optin_block/marketing-optin-block-frontend.asset.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (!defined('ABSPATH')) exit;
-
- return array('dependencies' => array('wc-blocks-checkout', 'wc-settings', 'wp-element', 'wp-polyfill'), 'version' => '1b6f40ff552dbb0279d88e42e21737a2');
